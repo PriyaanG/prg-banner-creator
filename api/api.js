889d7054c78e2233aa8d64ac47eb29a1ba147a3f
PriyaanG <168146157+PriyaanG@users.noreply.github.com>
2026-05-12 04:55:48 +0000

    API Test 4

--- api/api.js
+++ api/api.js
@@ -1,13 +1,11 @@
 export async function GET(request) {
-try {
     const resp = await fetch('https://tico09.com/photos/api/A320/DLH', {
       method: 'GET',
       headers: { 'Accept': 'application/json' },
     });
-    if (!resp.ok) return res.status(resp.status).json({ error: await resp.text() });
+    if (!resp.ok){ console.log(res.status(resp.status).json({ error: await resp.text() })); }
+    else {
     const data = await resp.json();
     return res.status(200).json(data);
-  } catch (err) {
-     console.log(res.status(500).json({ error: err.message }));
+    }
   }
-}
