f82c98e0ed75b7a511aa961d9427f18c19f5886e
PriyaanG <168146157+PriyaanG@users.noreply.github.com>
2026-05-18 00:07:19 +0000

    API Functionality For One Parameter

--- api/api.js
+++ api/api.js
@@ -6,6 +6,28 @@ export async function GET(request) {
   // Read a single param
   const plane = params.get('plane'); // returns string or null
   const airline = params.get('airline')
+
+  if (airline == "No Airport Filter Applied" && plane != "No Plane Filter Applied"){
+    const resp = await fetch(`https://tico09.com/photos/api/search?q=${plane}`, {
+      method: 'GET',
+      headers: { 'Accept': 'application/json' },
+    });
+    const data = await resp.json();
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
+  });
+  } else if (plane == "No Plane Filter Applied" && airline != "No Airline Filter Applied"){
+    const resp = await fetch(`https://tico09.com/photos/api/search?q=${airline}`, {
+      method: 'GET',
+      headers: { 'Accept': 'application/json' },
+    });
+    const data = await resp.json();
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
+  });
+  } else {
     const resp = await fetch(`https://tico09.com/photos/api/${plane}/${airline}`, {
       method: 'GET',
       headers: { 'Accept': 'application/json' },
@@ -16,5 +38,6 @@ export async function GET(request) {
     headers: { 'Content-Type': 'application/json' },
   });
   }
+}
 
 
