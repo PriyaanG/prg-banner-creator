4516667d809a4dde02b1853aa76fdb50a61b5aa9
PriyaanG <168146157+PriyaanG@users.noreply.github.com>
2026-05-25 00:03:44 +0000

    API Final Version

--- api/api.js
+++ api/api.js
@@ -6,6 +6,9 @@ export async function GET(request) {
   // Read a single param
   const plane = params.get('plane'); // returns string or null
   const airline = params.get('airline')
+  let responseList = []
+
+  let response;
 
   if (airline == ""){
     const resp = await fetch(`https://tico09.com/photos/api/search?q=${plane}`, {
@@ -13,31 +16,30 @@ export async function GET(request) {
       headers: { 'Accept': 'application/json' },
     });
     const data = await resp.json();
-  return new Response(JSON.stringify(data), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
-  });
+  response = data;
   } else if (plane == ""){
     const resp = await fetch(`https://tico09.com/photos/api/search?q=${airline}`, {
       method: 'GET',
       headers: { 'Accept': 'application/json' },
     });
     const data = await resp.json();
-  return new Response(JSON.stringify(data), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
-  });
+  response = data;
   } else {
     const resp = await fetch(`https://tico09.com/photos/api/${plane}/${airline}`, {
       method: 'GET',
       headers: { 'Accept': 'application/json' },
     });
     const data = await resp.json();
-  return new Response(JSON.stringify(data), {
+ response = data;
+  }
+  response.results.forEach(element => {
+  responseList.push({URL: element.url, Author: element.author})
+  });
+  console.log(responseList)
+    return new Response(JSON.stringify(responseList), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
   });
-  }
 }
 
 
