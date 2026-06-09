fb82299cb5922f5a8b0d9c45359e386b63ff42a9
PriyaanG <168146157+PriyaanG@users.noreply.github.com>
2026-06-09 04:12:31 +0000

    Cloudinary Upload API: More Bug Fixes

--- api/cloudinary-upload.js
+++ api/cloudinary-upload.js
@@ -1,6 +1,7 @@
+import { v2 as cloudinary } from 'cloudinary';
+
 export async function GET(request) {
 // Require the cloudinary library
-import { v2 as cloudinary } from 'cloudinary';
 const params = url.searchParams;
 const airlineVar = params.get('airline')
 const planeVar = params.get('plane')
