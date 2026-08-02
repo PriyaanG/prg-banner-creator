5a024dc6bb78e5b4e118ed6e02ea8e845b9c28b1
PriyaanG <168146157+PriyaanG@users.noreply.github.com>
2026-08-02 04:39:08 +0000

    Cloudinary Final???

--- api/cloudinary-upload.js
+++ api/cloudinary-upload.js
@@ -2,56 +2,26 @@ import { v2 as cloudinary } from 'cloudinary';
 
 export async function GET(request) {
 // Require the cloudinary library
-  const url = new URL(request.url);
 const params = url.searchParams;
 const airlineVar = params.get('airline')
 const planeVar = params.get('plane')
 const imagePath = params.get('imagepath')
 
-// Return "https" URLs by setting secure: true
-cloudinary.config({
-  secure: true
-});
-
-// Log the configuration
-console.log(cloudinary.config());
-
-/////////////////////////
-// Uploads an image file
-/////////////////////////
-const uploadImage = async (imagePath) => {
-
-    // Use the uploaded file's name as the asset's public ID and 
-    // allow overwriting the asset with new versions
-    const options = {
-      use_filename: true,
-      unique_filename: false,
-      overwrite: true,
-      metadata: `airline=${airlineVar}|plane=${planeVar}`
-    };
-
-    try {
-      // Upload the image
-      const result = await cloudinary.uploader.upload(imagePath, options);
-      console.log(result);
-
-        return new Response(JSON.stringify(result.secure_url), {
-         status: 200,
-         headers: { 'Content-Type': 'application/json' },
+cloudinary.uploader
+.upload("KLM.png", {
+  resource_type:"image",
+})
+.then((result) => {
+  return new Response(JSON.stringify("Sucess"), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
   });
-
-    } catch (error) {
-        return new Response(JSON.stringify(error), {
-        status: 500,
-        headers: { 'Content-Type': 'application/json' },
+})
+.catch((error) => {
+    return new Response(JSON.stringify("Error"), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
   });
-    }
-};
+})
 
-(async () => {
-    const publicId = await uploadImage(imagePath);
-    return new Response(JSON.stringify(publicId), {
-         status: 200,
-         headers: { 'Content-Type': 'application/json' },
-    }
-  )})}
+}
