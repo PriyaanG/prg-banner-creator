ee18ee172d973072b000072a01bdee1f99b4b812
PriyaanG <168146157+PriyaanG@users.noreply.github.com>
2026-08-04 07:50:32 +0000

    Perfect.

--- api/cloudinary-upload.js
+++ api/cloudinary-upload.js
@@ -7,13 +7,30 @@ export async function GET(request) {
   const airlineVar = params.get('airline');
   const planeVar = params.get('plane');
   const imagePath = params.get('imagepath');
-  const airportVar = params.get('airportvar')
+  const airportVar = params.get('airport')
+
+  if (imagePath == ""){
+     return new Response(JSON.stringify("Failed. Reason: Image Path Is Null"), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  } else if (planeVar == ""){
+    return new Response(JSON.stringify("Failed. Reason: Plane Variable Is Null"), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  } else if (airlineVar == "") {
+    return new Response(JSON.stringify("Failed. Reason: Airline Variable Is Null"), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 
   try {
     const result = await cloudinary.uploader.upload(imagePath, {
       resource_type: "image",
       metadata: `airline=${airlineVar}|plane=${planeVar}|airport=${airportVar}`,
-      public_id: `${planeVar}${airlineVar}_{airportVar}`
+      public_id: `${planeVar}${airlineVar}_${airportVar}`
     });
 
     return new Response(JSON.stringify("Success"), {
